90cebbea452bdc097d71e823f771eebd0ac1afe6
wkrdr_000 <wkrdr_000@Matt-Robotics>
2017-03-19 15:56:28 -0400

    Added linear motor acceleration

--- src/org/usfirst/frc/team3331/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team3331/robot/subsystems/DriveSubsystem.java
@@ -15,6 +15,10 @@ public class DriveSubsystem extends Subsystem {
 	public static double[] rollingLeftArray;
 	public static double[] rollingRightArray;
 	
+	public static double leftSpeed;
+	public static double rightSpeed;
+
+	
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
@@ -26,6 +30,9 @@ public class DriveSubsystem extends Subsystem {
 		// allocate memory for rolling-average arrays
 		rollingLeftArray = new double[ARRAY_DEPTH];
 		rollingRightArray = new double[ARRAY_DEPTH];
+		
+		leftSpeed = 0;
+		rightSpeed = 0;
 		
 		// initialize rolling-average arrays
 		for (int i = 0; i < ARRAY_DEPTH; i++) {
@@ -59,6 +66,7 @@ public class DriveSubsystem extends Subsystem {
     public void teleopDrive() {
     	double leftValue = RobotMap.gamepad.getRawAxis(RobotMap.leftStickY);
     	double rightValue = RobotMap.gamepad.getRawAxis(RobotMap.rightStickY);
+    	
     	
     	// This one needs shaft encoder feedback from the motors...
 		// Team 237's ramp:
@@ -102,8 +110,37 @@ public class DriveSubsystem extends Subsystem {
     		// rightValue = rightValue;
     		// leftValue = Math.copySign(Math.pow(leftValue, 2), leftValue);
     		// rightValue = Math.copySign(Math.pow(rightValue,2), rightValue);
+        	if (leftValue > 0.1 && leftSpeed < 1.0) {
+        		leftSpeed += 0.01;
+        	} else if (leftSpeed > 0 && leftValue < 0.1){
+        		leftSpeed -= 0.01;
+        	} 
+        	else if (leftValue < -0.1 && leftSpeed > -1.0) {
+        		leftSpeed -= 0.01;
+        	} else if (leftSpeed < 0 && leftValue > -0.1){
+        		leftSpeed += 0.01;
+        	} else if (leftSpeed < 0.1 && leftSpeed > -0.1) leftSpeed = 0;
+
+        	
+        	if (rightValue > 0.1 && rightSpeed < 1.0) {
+        		rightSpeed += 0.01;
+        	} else if (rightSpeed > 0 && rightValue < 0.1){
+        		rightSpeed -= 0.01;
+        	}
+        	else if (rightValue < -0.1 && rightSpeed < 1.0) {
+        		rightSpeed -= 0.01;
+        	} else if (rightSpeed < 0 && rightValue > -0.1){
+        		rightSpeed += 0.01;
+        	} else if (rightSpeed < 0.1 && rightSpeed > -0.1) rightSpeed = 0;
+        	leftValue = leftSpeed;
+        	rightValue = rightSpeed;
     	}
+    	// linear acceleration of the left and right motors
+
     	
+    	//RobotMap.drivetrain.tankDrive(leftSpeed, rightSpeed);
+    	
+    	// Feed the drivetrain the raw input values
     	RobotMap.drivetrain.tankDrive(leftValue, rightValue);
     }
 }
